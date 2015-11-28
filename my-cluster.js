c844a511f61345b16c58295fe0e781506f0d3149
daru23 <daru015@gmail.com>
2015-11-28 19:08:06 +0100

    quantity of cores

--- my-cluster.js
+++ my-cluster.js
@@ -6,17 +6,25 @@ module.exports = {
 
         if (cluster.isMaster) {
             // Fork workers.
-            for (var i = 0; i < numCPUs; i++) {
-                cluster.fork();
+            console.log('CPU available '+ numCPUs);
+
+            if ( numCPUs > 1 ){
+
+                for (var i = 0; i < numCPUs; i++) {
+                    cluster.fork();
+                }
+
+                Object.keys(cluster.workers).forEach(function(id) {
+                    console.log("I am running with ID : "+cluster.workers[id].process.pid);
+                });
+
+                cluster.on('exit', function(worker, code, signal) {
+                    console.log('worker ' + worker.process.pid + ' died');
+                });
+
+
             }
 
-            Object.keys(cluster.workers).forEach(function(id) {
-                console.log("I am running with ID : "+cluster.workers[id].process.pid);
-            });
-
-            cluster.on('exit', function(worker, code, signal) {
-                console.log('worker ' + worker.process.pid + ' died');
-            });
 
         }else {
             console.log('I am a worker! ');
